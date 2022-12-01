b2409f5fef59392f3fbca3ea24ccf33e8dd6a048
Riley Evans <riley@rileyevans.co.uk>
2022-12-01 07:47:26 +0000

    Day1

--- src/Day1.hs
+++ src/Day1.hs
@@ -6,12 +6,36 @@ module Day1
 import           Common                         ( inputFileName
                                                 , runTasksLines
                                                 )
+import           Data.List                      ( sortBy )
+import           Data.Ord                       ( Down(..)
+                                                , comparing
+                                                )
+import           Text.Read                      ( readMaybe )
 
-task1 :: [Int] -> IO (Maybe Int)
-task1 _ = return Nothing
+-- | Group inputs into each elf
+organiseInputs :: [Maybe Int] -> [[Int]]
+organiseInputs = foldr f k
+  where
+    f :: Maybe Int -> [[Int]] -> [[Int]]
+    f (Just x) []         = [[x]]
+    f (Just x) (xs : xss) = (x : xs) : xss
+    f Nothing  xss        = [] : xss
+    k = []
 
-task2 :: [Int] -> IO (Maybe Int)
-task2 _ = return Nothing
+
+
+
+task1 :: [Maybe Int] -> IO (Maybe Int)
+task1 messyInputs = do
+    let inputs = organiseInputs messyInputs
+    let values = map sum inputs
+    return (Just (maximum values))
+
+task2 :: [Maybe Int] -> IO (Maybe Int)
+task2 messyInputs = do
+    let inputs = organiseInputs messyInputs
+    let values = sortBy (comparing Down) (map sum inputs)
+    return (Just (sum (take 3 values)))
 
 -- Does not need to be read, this is just some way to manipulate each line of the input file
-run = runTasksLines (inputFileName 'run) read task1 task2
+run = runTasksLines (inputFileName 'run) readMaybe task1 task2
--- test/Day1Tests.hs
+++ test/Day1Tests.hs
@@ -17,14 +17,44 @@ task1tests :: TestTree
 task1tests = testGroup
     "task1 should"
     [ testCase "example1" $ do
-          res <- Day.task1 []
-          res @?= Nothing
+          res <- Day.task1
+              [ Just 1000
+              , Just 2000
+              , Just 3000
+              , Nothing
+              , Just 4000
+              , Nothing
+              , Just 5000
+              , Just 6000
+              , Nothing
+              , Just 7000
+              , Just 8000
+              , Just 9000
+              , Nothing
+              , Just 10000
+              ]
+          res @?= Just 24000
     ]
 
 task2tests :: TestTree
 task2tests = testGroup
     "task2 should"
     [ testCase "example1" $ do
-          res <- Day.task2 []
-          res @?= Nothing
+          res <- Day.task2
+              [ Just 1000
+              , Just 2000
+              , Just 3000
+              , Nothing
+              , Just 4000
+              , Nothing
+              , Just 5000
+              , Just 6000
+              , Nothing
+              , Just 7000
+              , Just 8000
+              , Just 9000
+              , Nothing
+              , Just 10000
+              ]
+          res @?= Just 45000
     ]
